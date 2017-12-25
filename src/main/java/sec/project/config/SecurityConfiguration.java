5fb44781c2412aff3fa30321193b73716e3d6f4c
juuso <ohtonen.juuso@gmail.com>
2017-12-25 22:26:20 +0200

    A few OWASP security flaws for https://cybersecuritybase.github.io/project/

--- src/main/java/sec/project/config/SecurityConfiguration.java
+++ src/main/java/sec/project/config/SecurityConfiguration.java
@@ -20,9 +20,25 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        // no real security at the moment
+        // OWASP: Top 10 2013-A8-Cross-Site Request Forgery (CSRF)
+        // http://resources.infosecinstitute.com/csrf-proof-of-concept-with-owasp-zap/#gref
+        // CSRF token is included in the forms, you then just have to enable the token validation here by commenting out
+        // or removing the following line
+        http.csrf().disable();
+
+        // OWASP: Top 10 2013-A7-Missing Function Level Access Control
+        // also the requests to pages describing signed up pages should be authorized, probably so that only the admin
+        // could view the content. The fix would be to include http://www.baeldung.com/spring-security-expressions-basic
+        // so that the line would include
+        //      .hasRole("ADMIN")
+        // Another fix could be to totally remove the lines. This would fix
+        // OWASP: Top 10 2013-A4-Insecure Direct Object References
+        // This issue means that "page/user" displays the name and address information for the specific page/user.
+        // a malicious user may find out the address information of a user if she can guess the user's name correctly.
+        http.authorizeRequests().antMatchers("/page/**", "**/page/**")
+                .permitAll();
         http.authorizeRequests()
-                .anyRequest().permitAll();
+                .anyRequest().authenticated().and().formLogin().permitAll();
     }
 
     @Autowired
--- src/main/java/sec/project/controller/SignupController.java
+++ src/main/java/sec/project/controller/SignupController.java
@@ -2,17 +2,36 @@ package sec.project.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 import sec.project.domain.Signup;
 import sec.project.repository.SignupRepository;
+
+import java.util.List;
 
 @Controller
 public class SignupController {
 
     @Autowired
     private SignupRepository signupRepository;
+
+    @RequestMapping(value={"/page/{name}"})
+    String pageMapping(Model model, @PathVariable("name") String name) {
+        List<Signup> all = signupRepository.findAll();
+        boolean isSet = false;
+        for (Signup signup : all) {
+            if (signup.getName().equals(name)) {
+                model.addAttribute("name", signup.getName());
+                model.addAttribute("address", signup.getAddress());
+                isSet = true;
+            }
+        }
+        if (!isSet) {
+            model.addAttribute("name", "Not found");
+            model.addAttribute("address", "Not found");
+        }
+        return "page";
+    }
 
     @RequestMapping("*")
     public String defaultMapping() {
@@ -25,9 +44,9 @@ public class SignupController {
     }
 
     @RequestMapping(value = "/form", method = RequestMethod.POST)
-    public String submitForm(@RequestParam String name, @RequestParam String address) {
+    public String submitForm(Model model, @RequestParam String name, @RequestParam String address) {
         signupRepository.save(new Signup(name, address));
+        model.addAttribute("url", "/page/" + name);
         return "done";
     }
-
 }
